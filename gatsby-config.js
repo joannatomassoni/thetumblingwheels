71407f17dce3e50f4fb1a8a14f4e00122b2bf300
Joanna Tomassoni <jctomassoni@gmail.com>
2020-04-08 18:28:22 -0500

    Set up gatsby-config with sass and font plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,4 +6,33 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: 'The Tumbling Wheels',
+    author: 'Joanna Tomassoni',
+  },
+  plugins: [
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: 'Jacques Francois Shadow' 
+          },
+          {
+            family: 'Cardo'
+          },
+          {
+            family: 'PT Serif'
+          },
+          {
+            family: 'Merriweather'
+          },
+          {
+            family: 'Inter'
+          }
+        ]
+      }
+    },
+  ]
 }
