ee26db92fa058114398e249512e620a45546875b
Joanna Tomassoni <jctomassoni@gmail.com>
2020-04-29 17:32:03 -0500

    Add basic contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,18 @@
+/**
+ * no contact form
+ * 
+ * email, social media
+ */
+
+import React from 'react';
+import Layout from '../components/layout'
+
+const ContactPage = () => {
+    return (
+        <Layout>
+            <p>Please reach out at <a href='mailto:thetumblingwheels@gmail.com'>thetumblingwheels@gmail.com</a></p>
+        </Layout>
+    )
+}
+
+export default ContactPage;
