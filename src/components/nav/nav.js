80fcab4d0efe7f3ca2631d47f4bdb9b16bde4c86
Joanna Tomassoni <jctomassoni@gmail.com>
2020-04-29 17:00:53 -0500

    Finalize basic nav

---
+++ src/components/nav/nav.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import navStyles from './nav.module.scss'
+
+const Nav = () => {
+	
+	return (
+		<div className={navStyles.header}>
+			<input type='checkbox' className={navStyles.toggler}/>
+			<div className={navStyles.hamburger}><div></div></div>
+				<nav className={navStyles.nav}>
+					<ul className={navStyles.navItems}>
+						<Link to='/' activeStyle={{ color: '#fff2df' }} className={navStyles.navItem} activeClassName={navStyles.activeNavItem}>Home</Link>
+						<a href='https://thetumblingwheels.bandcamp.com/' className={navStyles.navItem} activeClassName={navStyles.activeNavItem}>Music</a>
+						<Link to='/videos' className={navStyles.navItem} activeClassName={navStyles.activeNavItem}>Videos</Link>
+						<Link to='/contact' className={navStyles.navItem} activeClassName={navStyles.activeNavItem}>Contact</Link>
+					</ul>
+				</nav>
+
+		</div>
+	)
+}
+
+export default Nav;
