92fe542a8b41c53a5afa74c73d0a2c40e8bae13a
Joanna Tomassoni <jctomassoni@gmail.com>
2020-04-29 17:41:37 -0500

    Add basic contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,16 +1,12 @@
-/**
- * no contact form
- * 
- * email, social media
- */
-
 import React from 'react';
 import Layout from '../components/layout'
+import contactStyles from './contact.module.scss'
 
 const ContactPage = () => {
     return (
         <Layout>
-            <p>Please reach out at <a href='mailto:thetumblingwheels@gmail.com'>thetumblingwheels@gmail.com</a></p>
+            <p className={contactStyles.mainText}>Please reach out at <a href='mailto:thetumblingwheels@gmail.com'>thetumblingwheels@gmail.com</a>,</p>
+            <p className={contactStyles.mainTextTwo}>or find us on <a href='https://www.facebook.com/thetumblingwheels'>Facebook</a> or <a href='https://www.instagram.com/thetumblingwheels/'>Instagram</a></p>
         </Layout>
     )
 }
