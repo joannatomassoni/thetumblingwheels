5038614e954123f99426221f60f0f30cee9527d5
Joanna Tomassoni <jctomassoni@gmail.com>
2020-04-29 16:58:42 -0500

    Add video data

---
+++ src/assets/data/videos.js
@@ -0,0 +1,32 @@
+const VideoData = [
+    {
+        id: 0,
+        index: 0,
+        title: 'Rotten Town Live at the PinChurch',
+        author: 'Mystic Krewe',
+        url: 'https://www.youtube.com/embed/G4KRmIBG1Gk',
+    },
+    {
+        id: 1,
+        index: 1,
+        title: 'OnBeat Sessions',
+        author: 'Offbeat Magazine',
+        url: 'https://www.youtube.com/embed/bf6I9-USJ1A',
+    },
+    {
+        id: 2,
+        index: 2,
+        title: 'OnBeat Sessions',
+        author: 'Offbeat Magazine',
+        url: 'https://www.youtube.com/embed/bf6I9-USJ1A',
+    },
+    {
+        id: 3,
+        index: 3,
+        title: 'OnBeat Sessions',
+        author: 'Offbeat Magazine',
+        url: 'https://www.youtube.com/embed/bf6I9-USJ1A',
+    }
+]
+
+export default VideoData
