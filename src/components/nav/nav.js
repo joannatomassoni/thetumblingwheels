cf1f9c2f007f90cf3dbee937cdd3992a7e89ed15
Joanna Tomassoni <jctomassoni@gmail.com>
2020-05-25 11:46:19 -0500

    Get rid of videos link in nav

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -12,7 +12,6 @@ const Nav = () => {
 					<ul className={navStyles.navItems}>
 						<Link to='/' activeStyle={{ color: '#fff2df' }} className={navStyles.navItem} activeClassName={navStyles.activeNavItem}>Home</Link>
 						<a href='https://thetumblingwheels.bandcamp.com/' target='blank' className={navStyles.navItem} activeClassName={navStyles.activeNavItem}>Music</a>
-						<Link to='/videos' className={navStyles.navItem} activeClassName={navStyles.activeNavItem}>Videos</Link>
 						<Link to='/contact' className={navStyles.navItem} activeClassName={navStyles.activeNavItem}>Contact</Link>
 					</ul>
 				</nav>
