3e4b501f101ec67f535a546012d3e3e894eb42f3
Joanna Tomassoni <jctomassoni@gmail.com>
2020-05-25 11:46:43 -0500

    Touch up Contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,7 +6,7 @@ const ContactPage = () => {
     return (
         <Layout>
             <p className={contactStyles.mainText}>Please reach out at <a href='mailto:thetumblingwheels@gmail.com'>thetumblingwheels@gmail.com</a>,</p>
-            <p className={contactStyles.mainTextTwo}>or find us on <a href='https://www.facebook.com/thetumblingwheels'>Facebook</a> or <a href='https://www.instagram.com/thetumblingwheels/'>Instagram</a></p>
+            <p className={contactStyles.mainTextTwo}>or find us on <a href='https://www.facebook.com/thetumblingwheels'>Facebook</a> or <a href='https://www.instagram.com/thetumblingwheels/'>Instagram.</a></p>
         </Layout>
     )
 }
