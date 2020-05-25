e07219541e52c00eaea4989d79aaf2d1dd3131e1
Joanna Tomassoni <jctomassoni@gmail.com>
2020-05-25 11:38:49 -0500

    Opens new tab when clicking on booking email

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,7 +4,7 @@ import footerStyles from './footer.module.scss'
 const Footer = () => {
     return (
         <p className={footerStyles.contact}>
-            Booking: <a href='mailto:thetumblingwheels@gmail.com'>thetumblingwheels@gmail.com</a>
+            Booking: <a href='mailto:thetumblingwheels@gmail.com' target='_blank' rel='noreferrer'>thetumblingwheels@gmail.com</a>
         </p>
     )
 }
