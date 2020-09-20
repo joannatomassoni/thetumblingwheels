da9af4f94609b50a031f5eb7abe0cee8a0d9fa50
Joanna Tomassoni <jctomassoni@gmail.com>
2020-09-20 14:02:12 -0400

    Add Google Anaylitics tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,6 +33,21 @@ module.exports = {
           }
         ]
       }
-    }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-78794744-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: false,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Defers execution of google analytics script after page load
+        defer: false,
+      },
+    },
   ]
 }
